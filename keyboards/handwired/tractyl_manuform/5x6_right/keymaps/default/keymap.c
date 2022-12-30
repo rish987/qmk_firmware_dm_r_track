bbd276168d8a2ecda7bb4fa1ca9311254f247c37
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2022-12-30 05:06:48 +0000

    save

--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/default/keymap.c
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/default/keymap.c
@@ -39,28 +39,28 @@ enum custom_layers {
 #define RSFTBSPC  RSFT_T(KC_BSPC)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_5x6_right(
-     KC_Q   , KC_W  , KC_E     , KC_R  , KC_T  ,                         KC_Y  , KC_U     , KC_I     , KC_O  , KC_P ,
-     KC_A   , KC_S  , KC_D     , KC_F  , KC_G  ,                         KC_H  , KC_J     , KC_K     , KC_L  , KC_N ,
-     ESCCTL , KC_X  , KC_C     , KC_V  ,                                         KC_M     , KC_B     , KC_Z  , ESCCTL ,
-     KC_LSFT, RAISE , KC_SPACE , LOWER ,_______,                                 _______  , KC_ENTER   ,       RSFTBSPC
+     KC_Q   , KC_W  , KC_E     , KC_R  , KC_T  ,                         KC_Y   , KC_U     , KC_I     , KC_O  , KC_P ,
+     KC_A   , KC_S  , KC_D     , KC_F  , KC_G  ,                         KC_H   , KC_J     , KC_K     , KC_L  , KC_N ,
+     ESCCTL , KC_X  , KC_C     , KC_V  ,                                          KC_M     , KC_B     , KC_Z  , ESCCTL ,
+     KC_LSFT, RAISE , KC_SPACE , ARROW , LOWER ,                         TRKSWCH, _______  , KC_ENTER   ,       RSFTBSPC
   ),
 
   [_LOWER] = LAYOUT_5x6_right(
      _______,_______,_______,_______,KC_PERC,                        KC_CIRC,_______,_______,_______,_______,
      KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_5   ,                        KC_6   ,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,
      KC_1   ,KC_2   ,KC_3   ,KC_4   ,                                        KC_7   ,KC_8   ,KC_9   ,KC_0   ,
-     _______,_______,_______,_______,_______,                                _______,_______,        _______
+     _______,_______,_______,_______,_______,                        _______,_______,_______,        _______
   ),
   [_RAISE] = LAYOUT_5x6_right(
-     _______,_______,_______,_______,_______,                        _______,KC_SLSH,KC_TAB ,KC_LCBR,KC_RCBR,
-     _______,_______,_______,_______,_______,                        KC_GRV ,KC_EQL ,KC_QUOT,KC_BSLS,KC_SCLN,
-     _______,_______,_______,_______,                                        KC_MINS,KC_COMM,KC_DOT ,_______,
-     KC_RGUI ,_______,KC_RALT ,_______,_______,                              _______,ARROW  ,_______
+     _______,_______,KC_PLUS,KC_EQL ,KC_PIPE,                       KC_TILD,_______,_______,_______,_______,
+     KC_QUES,KC_LCBR,KC_RCBR,KC_UNDS,KC_BSLS,                       KC_GRV ,KC_DQT ,KC_COMM,KC_DOT ,KC_SCLN,
+     KC_SLSH,KC_LBRC,KC_RBRC,KC_MINS,                                       KC_QUOT,KC_LT  ,KC_GT  ,KC_COLN,
+     _______ ,_______,_______ ,_______,_______,                      _______,_______,_______,_______
   ),
   [_ARROW] = LAYOUT_5x6_right(
      KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                        KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,
      _______,_______,_______,_______,_______,                        KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,_______,
      _______,_______,_______,_______,                                        _______,KC_F11 ,KC_F12 ,_______,
-     _______,_______,_______,_______,_______,                                _______,_______,        _______
+     _______,_______,_______,_______,_______,                        _______,_______,KC_RGUI,        KC_RALT 
   ),
 };
