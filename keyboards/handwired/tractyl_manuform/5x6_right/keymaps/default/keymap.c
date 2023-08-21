9d9e87b5a82fb882a152b8e252f1b4665a39517f
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2023-08-21 21:44:24 +0200

    more comfortable ctrl placement

--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/default/keymap.c
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/default/keymap.c
@@ -34,8 +34,8 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_5x6_right(
      KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T  ,                         KC_Y   ,KC_U   ,KC_I   ,KC_O   , KC_P   ,
-     KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G  ,                         KC_H   ,KC_J   ,KC_K   ,KC_L   , KC_N   ,
-     BSPCTLN,KC_X   ,KC_C   ,KC_V   ,                                TRKSWCH,KC_M   ,KC_B   ,KC_Z   , ESCCTLN,
+     BSPCCTL,KC_S   ,KC_D   ,KC_F   ,KC_G  ,                         KC_H   ,KC_J   ,KC_K   ,KC_L   , ESCCTL ,
+     KC_Z   ,KC_A   ,KC_C   ,KC_V   ,                                TRKSWCH,KC_M   ,KC_B   ,KC_N   , KC_X   ,
      RAISE  ,ARROW  ,SPCNI3 ,_______,LOWER ,                                 TD(STABCTL),TD(SALTENT)
   ),
   [_LOWER] = LAYOUT_5x6_right(
